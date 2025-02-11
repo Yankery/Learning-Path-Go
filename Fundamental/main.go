72bc7c843efc8ec82282969566b76ec462653255
Yankery <trang2003bh@gmail.com>
2025-02-11 12:22:08 +0700

    finish Fundamental

--- Fundamental/main.go
+++ Fundamental/main.go
@@ -2,18 +2,73 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func main() {
-	//Simple data types
-	name, age := "David", 12
-	employee := []string{"Arthur", "David", "Carter"}
-	ages := []int{11, 12, 13}
+	// name, age := "David", 12
 
-	fmt.Println(name, ": ", age)
-	fmt.Println(strings.Repeat("-", 15))
-	for i := 0; i < len(employee); i++ {
-		fmt.Println(employee[i], ": ", ages[i])
+	// employee := []string{"Arthur", "David", "Carter"}
+	// ages := []int{21, 22, 23}
+
+	// employeeMap := map[string]int{
+	// 	employee[0]: ages[0],
+	// 	employee[1]: ages[1],
+	// 	employee[2]: ages[2],
+	// }
+
+	type form struct {
+		name string
+		age  int
+	}
+
+	employeeStruct := []form{
+		{"Arthur", 21},
+		{"David", 22},
+		{"Carter", 23},
+	}
+
+	// fmt.Println(name, ": ", age)
+	// fmt.Println(strings.Repeat("-", 15))
+
+	// fmt.Print("Please type employee name: ")
+	// var option string
+	// fmt.Scan(&option)
+
+	//If statement
+	// if option == "Arthur" {
+	// 	fmt.Println("Arthur", ": ", employeeMap["Arthur"])
+	// } else if option == "David" {
+	// 	fmt.Println("David", ": ", employeeMap["David"])
+	// } else if option == "Carter" {
+	// 	fmt.Println("Carter", ": ", employeeMap["Carter"])
+	// } else {
+	// 	fmt.Println("Employee doesn't exist!")
+	// }
+
+	//Switch statement
+	// val, ok := employeeMap[option]
+	// switch ok {
+	// case true:
+	// 	fmt.Println(option, ": ", val)
+	// default:
+	// 	fmt.Println("Employee doesn't exist!")
+	// }
+
+	//infinite for loop
+	// i := 0
+	// for {
+	// 	fmt.Println(i)
+	// 	i += 1
+	// }
+
+	//full conditional for loop
+	// for i := 0; i < len(employee); i++ {
+	// 	fmt.Println(employee[i], ": ", ages[i])
+	// }
+	// fmt.Println(strings.Repeat("-", 15))
+
+	//collection-based for loop
+	for _, value := range employeeStruct {
+		fmt.Println(value.name, ": ", value.age)
 	}
 }
