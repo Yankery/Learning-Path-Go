8e2bdbda5c43826515ceb4b0a233ba1653c17abe
Yankery <trang2003bh@gmail.com>
2025-02-21 15:14:47 +0700

    add 1 version for mutex

--- Concurrent/Mutexes/main.go
+++ Concurrent/Mutexes/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 // not practical, since there is no need for goroutine if all go sequen
 func main() {
+	startNow := time.Now()
 	s := []int{}
 	var wg sync.WaitGroup
 	var m sync.Mutex
@@ -24,4 +26,27 @@ func main() {
 	wg.Wait()
 
 	fmt.Println(len(s))
+
+	// var wg sync.WaitGroup
+	// const iteration = 1000
+	// dataChan := make(chan int)
+
+	// for i := 0; i < iteration; i++ {
+	// 	wg.Add(1)
+	// 	go func() {
+	// 		defer wg.Done()
+	// 		dataChan <- 1
+	// 	}()
+	// }
+	// go func() {
+	// 	wg.Wait()
+	// 	close(dataChan)
+	// }()
+	// base := 0
+	// for num := range dataChan {
+	// 	base += num
+	// }
+
+	// fmt.Println(base)
+	fmt.Println("The operation took: ", time.Since(startNow))
 }
